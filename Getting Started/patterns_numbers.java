0ce47740d129902d4fc274e1f9979e41601e7102
Saumya <91318429+saum29@users.noreply.github.com>
2022-02-12 14:01:48 +0530

    Add files via upload

--- Getting Started/patterns_numbers.java
+++ Getting Started/patterns_numbers.java
@@ -130,13 +130,126 @@ public class patterns_numbers {
         }
     }
 
-    private static void pattern7(int n) {
+    private static void pattern7(int n) { // pascals right triangle
+        for (int r = 1; r <= n; r++) {
+            // Variable used to represent C(r, i)
+            int C = 1;
+            for (int cst = 1; cst <= r; cst++) {
+                // The first value in a line is always 1
+                System.out.print(C + " ");
+
+                C = C * (r - cst) / cst;
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern8(int n) { // + types
         int nst = 1;
+        int nsp = n / 2;
         for (int r = 0; r < n; r++) {
-            for (int cst = 0; cst < nst; cst++) {
+            for (int csp = 0; csp < nsp && r != n / 2; csp++) {
+                System.out.print("\t");
+            }
+
+            for (int cst = 0; cst < nst && r != n / 2; cst++) {
                 System.out.print("*\t");
             }
-            nst += 1;
+            if (r == n / 2) {
+                for (int i = 0; i < n; i++) {
+                    System.out.print("*\t");
+                }
+            }
+
+            if (r < n / 2) {
+                nst += 1;
+            } else {
+                nst -= 1;
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern9(int n) {// hour glass
+        int nst = n;
+        int nsp1 = 0;
+        for (int r = 0; r < n; r++) {
+            for (int csp = 0; csp < nsp1; csp++) {
+                System.out.print("\t");
+            }
+
+            for (int cst = 0; cst < nst; cst++) {
+                if (r > 0 && r < n / 2 && cst > 0 && cst < nst - 1)
+                    System.out.print("\t");
+                else
+                    System.out.print("*\t");
+            }
+
+            if (r < n / 2) {
+                nsp1 += 1;
+                nst -= 2;
+            } else {
+                nsp1 -= 1;
+                nst += 2;
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern10(int n) {// inverted hour glass
+        int nst = n;
+        int nsp1 = 0;
+        for (int r = 0; r < n; r++) {
+            for (int csp = 0; csp < nsp1; csp++) {
+                System.out.print("\t");
+            }
+
+            for (int cst = 0; cst < nst; cst++) {
+                if (r > n / 2 && r < n - 1 && cst > 0 && cst < nst - 1)
+                    System.out.print("\t");
+                else
+                    System.out.print("*\t");
+            }
+
+            if (r < n / 2) {
+                nsp1 += 1;
+                nst -= 2;
+            } else {
+                nsp1 -= 1;
+                nst += 2;
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern11(int n) { // M
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (c == 0 || c == n - 1)
+                    System.out.print("*\t");
+
+                else if (r <= n / 2 && (r == c || r + c == n - 1))
+                    System.out.print("*\t");
+
+                else
+                    System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern12(int n) { // W
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (c == 0 || c == n - 1)
+                    System.out.print("*\t");
+
+                else if (r >= n / 2 && (r == c || r + c == n - 1))
+                    System.out.print("*\t");
+
+                else
+                    System.out.print("\t");
+            }
             System.out.println();
         }
     }
@@ -144,7 +257,7 @@ public class patterns_numbers {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
-            pattern7(n);
+            pattern12(n);
         }
     }
 }
