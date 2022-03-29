8145c568d336b9e06d5c019a3675a3c753a51c20
Saumya <91318429+saum29@users.noreply.github.com>
2022-03-29 18:18:05 +0530

    Add files via upload

---
+++ Tree/Construction.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class Construction {
+
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static class Node {
+        int val;
+        Node left;
+        Node right;
+
+        Node() {
+        }
+
+        Node(int val) {
+            this.val = val;
+        }
+
+        Node(int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] data = { 1, 2, 3, 4, null, null };
+
+        Node root = new Node(arr[0]);
+        Pair rtp = new Pair(root, 1);
+
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+
+        int idx = 0;
+        while (st.size() > 0) {
+            Pair top = st.peek();
+
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    Node ln = new Node(arr[idx], null, null);
+                    top.node.left = ln;
+
+                    Pair lp = new Pair(ln, 1);
+                    st.push(lp);
+                }
+
+                else {
+                    top.node.left = null;
+                }
+
+                top.state++;
+
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    Node rn = new Node(arr[idx], null, null);
+                    top.node.right = rn;
+
+                    Pair rp = new Pair(rn, 1);
+                    st.push(rp);
+                }
+
+                else {
+                    top.node.right = null;
+                }
+
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+
+    }
+}
